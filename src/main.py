ac4fdb93513f6348dc7bf5e15c552e79e776f06a
Léa <140352752+LH-90@users.noreply.github.com>
2024-07-22 18:48:10 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from atm import ATM
 
 def main():
     atm = ATM(pin_user=1062, balance_user=1879)
-    atm.verify_pin()  # Calls the function to verify PIN
+    atm.verify_pin()  # Calls the method to verify PIN
 
 if __name__ == "__main__":
     main()
